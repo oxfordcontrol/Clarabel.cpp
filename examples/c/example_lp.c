b86ecbabf9d7fae8943979ca03f05814267f8fd3
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-03 16:34:07 +0100

    Add C example

--- examples/c/example_lp.c
+++ examples/c/example_lp.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "clarabel.h"
 
 int main(void)
 {
     printf("Hello, world!\n");
+
+    void *_A = CscMatrix_new(
+        4, 2,                            // row, col
+        (uintptr_t[]){0, 2, 4},          // colptr
+        (uintptr_t[]){0, 2, 1, 3},       // rowval
+        (double[]){1.0, -1.0, 1.0, -1.0} // nzval
+    );
+
     return 0;
 }
