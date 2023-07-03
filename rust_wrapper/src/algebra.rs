d9ee7b232ac0a48c7c165811140b72cb5c7af736
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-03 15:06:24 +0100

    Add CscMatrix::new

---
+++ rust_wrapper/src/algebra.rs
@@ -0,0 +1,30 @@
+use clarabel::algebra as lib;
+use std::{ffi::c_void, usize};
+
+//pub type CscMatrixF64 = c_void;
+
+#[no_mangle]
+pub extern "C" fn CscMatrix_new(
+    m: usize,
+    n: usize,
+    colptr: *const usize,
+    rowval: *const usize,
+    nzval: *const f64,
+) -> *mut c_void {
+    // Recover the vectors from the raw array pointers
+    
+    // Length of colptr is n + 1
+    let colptr_vec = unsafe { Vec::from_raw_parts(colptr as *mut usize, n + 1, n + 1) };
+    let num_of_nzvalues = colptr_vec[n]; // Length of rowval and nzval is colptr[n]
+    let rowval_vec = unsafe { Vec::from_raw_parts(rowval as *mut usize, num_of_nzvalues, num_of_nzvalues) };
+    let nzval_vec = unsafe { Vec::from_raw_parts(nzval as *mut f64, num_of_nzvalues, num_of_nzvalues) };
+
+    // Call the CscMatrix constructor defined in Rust
+    let matrix = lib::CscMatrix::<f64>::new(m, n, colptr_vec, rowval_vec, nzval_vec);
+    
+    // Return a raw pointer to the boxed lib::Cscmatrix<f64> object
+    // The matrix is allocated on the heap and the pointer is returned for C to use
+    let boxed = Box::new(matrix);
+    let ptr: *mut lib::CscMatrix<f64> =Box::into_raw(boxed); 
+    ptr as *mut c_void
+}
