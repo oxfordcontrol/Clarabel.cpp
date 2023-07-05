ce0b15d3ae3c379066793d52ed5e0d2451d35769
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-05 10:40:19 +0100

    Update example

--- examples/c/example_lp.c
+++ examples/c/example_lp.c
@@ -6,6 +6,11 @@ int main(void)
     printf("Hello, world!\n");
 
     // Allocate a new CscMatrixF64
+
+    // TODO: CscMatrix_f64 *P = CscMatrix::<f64>::zeros((2, 2));
+
+    double q[2] = {1.0, -1.0};
+
     CscMatrix_f64 *_A = CscMatrix_f64_new(
         4, 2,                            // row, col
         (uintptr_t[]){0, 2, 4},          // colptr
@@ -13,7 +18,24 @@ int main(void)
         (double[]){1.0, -1.0, 1.0, -1.0} // nzval
     );
 
-    // Delete the CscMatrixF64 object
+    double b[4] = {1.0, 1.0, 1.0, 1.0};
+
+    // Build solver
+    DefaultSolver *solver = DefaultSolver_new(
+        NULL,   // P
+        q,      // q
+        _A,     // A
+        b,      // b
+        1,      // TODO: n_cones
+        NULL,   // TODO: cones
+        NULL    // TODO: settings
+    );
+
+    // Solve
+    DefaultSolver_solve(solver);
+
+    // Free the matrices and the solver
+    free_DefaultSolver(solver);
     free_CscMatrix_f64(_A);
 
     return 0;
