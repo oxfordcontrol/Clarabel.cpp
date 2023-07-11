e6d34c2d6b3f75ae6dbad5d6f94b33cd6fe41e9c
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-11 11:59:01 +0100

    Update example_lp

--- examples/c/example_lp.c
+++ examples/c/example_lp.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include "clarabel.h"
 
 int main(void)
 {
-    printf("Hello, world!\n");
-
     CscMatrix_f64 *P = CscMatrix_f64_zeros(2, 2);
-
     double q[2] = {1.0, -1.0};
 
     // a 2-d box constraint, separated into 4 inequalities.
@@ -18,20 +14,24 @@ int main(void)
         (double[]){1.0, -1.0, 1.0, -1.0} // nzval
     );
 
-    // easier way - use the From trait to construct A:
+    // easier way - construct A from a dense matrix
     CscMatrix_f64 *A = CscMatrix_f64_from(
         4, 2,
-        (double[4][2]){
+        (double[4][2])
+        {
             {1.0, 0.0},
             {0.0, 1.0},
             {-1.0, 0.0},
-            {0.0, -1.0}});
+            {0.0, -1.0}
+        }
+    );
 
     double b[4] = {1.0, 1.0, 1.0, 1.0};
 
     SupportedConeT_f64 cones[1] =
-        {
-            NonnegativeConeT_f64(4)};
+    {
+        NonnegativeConeT_f64(4)
+    };
 
     // Settings
     DefaultSettings_f64 settings = DefaultSettingsBuilder_f64_default();
