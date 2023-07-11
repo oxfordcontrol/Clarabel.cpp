1f53d51d728a5cb1d37f611463ac4accbd2dc4bd
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-11 12:43:11 +0100

    Add example_socp

---
+++ examples/c/example_socp.c
@@ -0,0 +1,58 @@
+#include "clarabel.h"
+
+int main(void)
+{
+    // SOCP Example
+
+    CscMatrix_f64 *P = CscMatrix_f64_from(
+        2, 2,
+        (double[2][2])
+        {
+            {0., 0.},
+            {0., 2.}
+        }
+    );
+
+    double q[2] = {0., 0.};
+
+    CscMatrix_f64 *A = CscMatrix_f64_from(
+        3, 2,
+        (double[3][2])
+        {
+            { 0.,  0.},  
+            {-2.,  0.}, 
+            { 0., -1.}, 
+        }
+    );
+
+    double b[3] = {1., -2., -2.};
+
+    SupportedConeT_f64 cones[1] =
+    {
+        SecondOrderConeT_f64(3)
+    };
+
+    // Settings
+    DefaultSettings_f64 settings = DefaultSettingsBuilder_f64_default();
+
+    // Build solver
+    DefaultSolver *solver = DefaultSolver_f64_new(
+        P, // P
+        q, // q
+        A, // A
+        b, // b
+        1, // n_cones
+        cones,
+        &settings
+    );
+
+    // Solve
+    DefaultSolver_solve(solver);
+
+    // Free the matrices and the solver
+    free_DefaultSolver(solver);
+    free_CscMatrix_f64(P);
+    free_CscMatrix_f64(A);
+
+    return 0;
+}
