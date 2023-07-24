6bbcb3c64d81ca313a53d5da233ccc427c8ab936
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-07-24 21:38:56 +0100

    Update C header

--- include/c/CscMatrix.h
+++ include/c/CscMatrix.h
@@ -47,65 +47,40 @@ typedef struct ClarabelCscMatrix_f32
     const float *nzval;
 } ClarabelCscMatrix_f32;
 
-/// @brief Create a sparse matrix in Compressed Sparse Column format
+/// @brief Initialize a sparse matrix in Compressed Sparse Column format
 /// @param m Number of rows
 /// @param n Number of columns
 /// @param colptr CSC format column pointer array (always have length n+1)
 /// @param rowval Array of row indices (always have length colptr[n])
 /// @param nzval Array of nonzero values (always have length colptr[n])
-/// @return Pointer to a new CscMatrix_f64 object allocated on the heap
-static inline ClarabelCscMatrix_f64 *clarabel_CscMatrix_f64_new(
+void clarabel_CscMatrix_f32_init(
+    ClarabelCscMatrix_f32 *ptr,
     uintptr_t m,
     uintptr_t n,
     const uintptr_t *colptr,
     const uintptr_t *rowval,
-    const double *nzval)
-{
-    ClarabelCscMatrix_f64 *ptr = malloc(sizeof(ClarabelCscMatrix_f64));
+    const float *nzval);
 
-    if (ptr == NULL) // Failed to allocate memory
-        return NULL;
-
-    // Initialize CscMatrix_f64
-    ptr->m = m;
-    ptr->n = n;
-    ptr->colptr = colptr;
-    ptr->rowval = rowval;
-    ptr->nzval = nzval;
-
-    return ptr;
-}
-
-static inline ClarabelCscMatrix_f32 *clarabel_CscMatrix_f32_new(
+/// @brief Initialize a sparse matrix in Compressed Sparse Column format
+/// @param m Number of rows
+/// @param n Number of columns
+/// @param colptr CSC format column pointer array (always have length n+1)
+/// @param rowval Array of row indices (always have length colptr[n])
+/// @param nzval Array of nonzero values (always have length colptr[n])
+void clarabel_CscMatrix_f64_init(
+    ClarabelCscMatrix_f64 *ptr,
     uintptr_t m,
     uintptr_t n,
     const uintptr_t *colptr,
     const uintptr_t *rowval,
-    const float *nzval)
-{
-    ClarabelCscMatrix_f32 *ptr = malloc(sizeof(ClarabelCscMatrix_f32));
-
-    if (ptr == NULL) // Failed to allocate memory
-        return NULL;
-
-    // Initialize CscMatrix_f32
-    ptr->m = m;
-    ptr->n = n;
-    ptr->colptr = colptr;
-    ptr->rowval = rowval;
-    ptr->nzval = nzval;
-
-    return ptr;
-}
+    const double *nzval);
 
 #ifdef CLARABEL_USE_FLOAT
 typedef ClarabelCscMatrix_f32 ClarabelCscMatrix;
-#define clarabel_CscMatrix_new(...) clarabel_CscMatrix_f32_new(__VA_ARGS__)
-#define clarabel_CscMatrix_free(...) clarabel_CscMatrix_f32_free(__VA_ARGS__)
+#define clarabel_CscMatrix_init(...) clarabel_CscMatrix_f32_init(__VA_ARGS__)
 #else
 typedef ClarabelCscMatrix_f64 ClarabelCscMatrix;
-#define clarabel_CscMatrix_new(...) clarabel_CscMatrix_f64_new(__VA_ARGS__)
-#define clarabel_CscMatrix_free(...) clarabel_CscMatrix_f64_free(__VA_ARGS__)
+#define clarabel_CscMatrix_init(...) clarabel_CscMatrix_f64_init(__VA_ARGS__)
 #endif /* CLARABEL_USE_FLOAT */
 
 #endif /* CLARABEL_CSC_MATRIX_H */
