2f5f5c793237f76bb892e95c30c1f64d034fc059
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-08-04 22:56:55 +0100

    Add basic_lp tests

--- tests/basic_lp.cpp
+++ tests/basic_lp.cpp
@@ -1,9 +1,10 @@
 #include <Clarabel>
 #include <Eigen/Eigen>
-#include <gtest/gtest.h>
-#include <vector>
 #include <cmath>
+#include <gtest/gtest.h>
 #include <iostream>
+#include <limits>
+#include <vector>
 
 using namespace std;
 using namespace clarabel;
@@ -29,8 +30,7 @@ class BasicLPTest : public ::testing::Test
         MatrixXd I1 = MatrixXd::Identity(3, 3);
         MatrixXd I2 = -MatrixXd::Identity(3, 3);
         MatrixXd A_dense = MatrixXd::Zero(6, 3);
-        A_dense <<
-            I1,
+        A_dense << I1,
             I2;
         A_dense *= 2.0;
         A = A_dense.sparseView();
@@ -38,7 +38,6 @@ class BasicLPTest : public ::testing::Test
     }
 };
 
-// Demonstrate some basic assertions.
 TEST_F(BasicLPTest, Feasible)
 {
     DefaultSettings<double> settings = DefaultSettingsBuilder<double>::default_settings().build();
@@ -63,5 +62,41 @@ TEST_F(BasicLPTest, Feasible)
     ASSERT_TRUE(actual_solution.isApprox(ref_solution, 1e-8));
 
     double ref_obj = -3.0;
-    // ASSERT_NEAR(solver.info().cost_primal, ref_obj, 1e-8);
+    ASSERT_NEAR(solver.info().cost_primal, ref_obj, 1e-8);
+}
+
+TEST_F(BasicLPTest, PrimalInfeasible)
+{
+    A.valuePtr()[1] = 1.0; // swap lower bound on first variable to redundant upper bound
+
+    c[0] = 1.0;
+    c[1] = 0.0;
+    c[2] = 0.0;
+
+    DefaultSettings<double> settings = DefaultSettingsBuilder<double>::default_settings().build();
+
+    clarabel::eigen::DefaultSolver<double> solver(P, c, A, b, cones, &settings);
+    solver.solve();
+
+    DefaultSolution<double> solution = solver.solution();
+    ASSERT_EQ(solution.status, SolverStatus::DualInfeasible);
+}
+
+TEST_F(BasicLPTest, InfeasibleIllCond)
+{
+    double *A_nzval = A.valuePtr();
+    A_nzval[0] = numeric_limits<double>::epsilon();
+    A_nzval[1] = -numeric_limits<double>::epsilon();
+
+    c[0] = 1.0;
+    c[1] = 0.0;
+    c[2] = 0.0;
+
+    DefaultSettings<double> settings = DefaultSettingsBuilder<double>::default_settings().build();
+
+    clarabel::eigen::DefaultSolver<double> solver(P, c, A, b, cones, &settings);
+    solver.solve();
+
+    DefaultSolution<double> solution = solver.solution();
+    ASSERT_EQ(solution.status, SolverStatus::DualInfeasible);
 }
