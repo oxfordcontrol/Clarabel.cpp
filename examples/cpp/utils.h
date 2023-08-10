315e1f1f19c58e2e893e4ad439844456c2e08aad
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2023-08-10 19:48:48 +0100

    Use eigen vector in DefaultSolution

--- examples/cpp/utils.h
+++ examples/cpp/utils.h
@@ -8,12 +8,13 @@
 namespace utils
 {
     template<typename T>
-    void print_array(T *array, size_t n)
+    void print_array(Eigen::Map<Eigen::VectorX<T>> &vec)
     {
         printf("[");
+        size_t n = vec.size();
         for (size_t i = 0; i < n; i++)
         {
-            printf("%.10f", array[i]);
+            printf("%.10f", vec.data()[i]);
             if (i < n - 1)
             {
                 printf(", ");
@@ -26,11 +27,11 @@ namespace utils
     void print_solution(clarabel::DefaultSolution<T> &solution)
     {
         printf("Solution (x)\t = ");
-        print_array(solution.x, solution.x_length);
+        print_array(solution.x);
         printf("Multipliers (z)\t = ");
-        print_array(solution.z, solution.z_length);
+        print_array(solution.z);
         printf("Slacks (s)\t = ");
-        print_array(solution.s, solution.s_length);
+        print_array(solution.s);
     }
 }
 
--- include/cpp/DefaultSolution.h
+++ include/cpp/DefaultSolution.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Eigen/Eigen>
 #include <cstdint>
 #include <type_traits>
 
@@ -21,22 +22,46 @@ namespace clarabel
     };
 
     template<typename T = double>
-    struct DefaultSolution
+    class DefaultSolution
     {
         static_assert(std::is_same<T, float>::value || std::is_same<T, double>::value, "T must be float or double");
 
-        T* x;
-        uintptr_t x_length;
-        T* z;
-        uintptr_t z_length;
-        T* s;
-        uintptr_t s_length;
+    public:
+        struct ClarabelDefaultSolution
+        {
+            T *x;
+            uintptr_t x_length;
+            T *z;
+            uintptr_t z_length;
+            T *s;
+            uintptr_t s_length;
+            SolverStatus status;
+            T obj_val;
+            double solve_time;
+            uint32_t iterations;
+            T r_prim;
+            T r_dual;
+        };
+
+        Eigen::Map<Eigen::VectorX<T>> x, z, s;
         SolverStatus status;
         T obj_val;
         double solve_time;
         uint32_t iterations;
         T r_prim;
         T r_dual;
+
+        DefaultSolution(ClarabelDefaultSolution &solution)
+            : x(solution.x, solution.x_length),
+              z(solution.z, solution.z_length),
+              s(solution.s, solution.s_length),
+              status(solution.status),
+              solve_time(solution.solve_time),
+              iterations(solution.iterations),
+              r_prim(solution.r_prim),
+              r_dual(solution.r_dual)
+        {
+        }
     };
 
     // Instantiate the templates
